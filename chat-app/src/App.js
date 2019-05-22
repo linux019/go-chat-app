485d163f5e92c0e1a0db5f5f334b3b740e2d8c61
developer <developer@localhost>
2019-05-22 23:20:05 +0300

    slice chat

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './App.scss';
-
+// Teddie Miller
+// Reece Sharp
+// Hamish Matthews
+// Charles Burns
+// Eric Palmer
+// Caelan Green
+// Wilfred Khan
+// Alex Allen
+// Charlie Stevens
+// Harley Robertson
 function App() {
     return (
         <>
-        <div className={'sidebar'}>
+            <div className='sidebar'>
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Card Title</span>
+                        <p>I am a very simple card</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">
+                            <i className={'material-icons tiny'}>check_circle</i>
+                            This is a link</a>
+                    </div>
+                </div>
 
-        </div>
-        <div className="dialogue">
-            content
+                <ul className="collection with-header">
+                    <li className="collection-header"><h6>First Names</h6></li>
+                    <li className="collection-item">Alvin</li>
+                    <li className="collection-item">Alvin</li>
+                    <li className="collection-item">Alvin</li>
+                    <li className="collection-item">Alvin</li>
+                </ul>
+            </div>
+            <div className="dialogue">
+                content
 
-        </div>
+            </div>
         </>
     );
 }
